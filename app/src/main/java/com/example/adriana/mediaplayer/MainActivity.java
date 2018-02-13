cb5b0cd267d4d9df02f4b1efae5624ac3ba856b4
AdrianaNeagu <adriana.neagu86@gmail.com>
2018-02-13 12:00:53 +0200

    added SeekBar used for manual Volume control

--- app/src/main/java/com/example/adriana/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/adriana/mediaplayer/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.adriana.mediaplayer;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1, b2;
+    TextView textVolume;
+    SeekBar seekbar;
     MediaPlayer mediaPlayer;
+    AudioManager audioManager;
 
 
     @Override
@@ -20,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = findViewById(R.id.play_button);
         b2 = findViewById(R.id.pause_button);
+        textVolume = findViewById(R.id.seekbar_volume_text);
+        seekbar = findViewById(R.id.seekbar);
 
         mediaPlayer = MediaPlayer.create(this, R.raw.song);
 
@@ -38,6 +47,26 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.pause();
             }
         });
+
+        audioManager= (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        seekbar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekbar, int i, boolean b) {
+                textVolume.setText("Media Volume : " + i);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
 
 
 
